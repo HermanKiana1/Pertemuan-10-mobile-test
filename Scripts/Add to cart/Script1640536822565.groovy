fb54a715084195d201743462302e8c00425f24b4
kb-fmf-kpm <kb-fmf-kpm@localhost>
2021-12-27 00:24:45 +0700

    add to cart

--- Scripts/Add to cart/Script1640536822565.groovy
+++ Scripts/Add to cart/Script1640536822565.groovy
@@ -1,3 +1,6 @@
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory
+import io.appium.java_client.AppiumDriver
+import com.kms.katalon.core.util.KeywordUtil
 import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
@@ -26,6 +29,15 @@ Mobile.tap(findTestObject('Object Repository/Add to cart/android.widget.Button -
 Mobile.setText(findTestObject('Object Repository/Add to cart/android.widget.EditText'), '1', 0)
 
 Mobile.tap(findTestObject('Object Repository/Add to cart/android.widget.Button - ADD'), 0)
+
+AppiumDriver<?> driver = MobileDriverFactory.getDriver()
+def toast = driver.findElementByXPath("//android.widget.Toast[@text='Success add Product to cart']")
+println("Toast element: " + toast)
+if (toast == null) {
+	KeywordUtil.markFailed('ERROR: Toast object not found!')
+}
+
+WebUI.delay(5)
 
 Mobile.closeApplication()
 
